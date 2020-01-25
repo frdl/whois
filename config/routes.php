3322c2c49506c6e0b4da4df79f74e37beb8486bc
Till Wehowski <github@webfan.de>
2020-01-25 06:20:56 +0100

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -2,9 +2,12 @@
 use function DI\add;
 //'GET|POST|PUT|DELETE|OPTIONS', '/bar/[:controller]/[:action].[:type]?', 'bar_action', 'bar_route'
 
- $preferences = json_decode(file_get_contents(__DIR__ .\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR.'preferences.json'));
+ //$preferences = json_decode(file_get_contents(__DIR__ .\DIRECTORY_SEPARATOR.'..'.\DIRECTORY_SEPARATOR.'preferences.json'));
 
- 
+   $f = rtrim(dirname(dirname(dirname(dirname(__FILE__)))), \DIRECTORY_SEPARATOR.'/ ')
+				   .\DIRECTORY_SEPARATOR.'config'.\DIRECTORY_SEPARATOR.'module-configs'.\DIRECTORY_SEPARATOR.basename(dirname(dirname(__FILE__))).\DIRECTORY_SEPARATOR.'preferences.json';
+
+        $preferences = json_decode(file_get_contents($f));
 
 return  [
 
